a49f698a6b924fc7280de318898bdd1dba029afd
liudaimei <liudaimei@cheweiguanjia.com>
2020-12-18 16:36:30 +0800

    no message

--- app/src/main/java/com/example/cwgj/alibabapush/MyMessageReceiver.java
+++ app/src/main/java/com/example/cwgj/alibabapush/MyMessageReceiver.java
@@ -1,24 +1,14 @@
 package com.example.cwgj.alibabapush;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.RemoteViews;
 
 import com.alibaba.sdk.android.push.MessageReceiver;
 import com.alibaba.sdk.android.push.notification.CPushMessage;
-import com.example.cwgj.pushlib.R;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
-
-import static android.content.Context.NOTIFICATION_SERVICE;
-import static com.alibaba.sdk.android.ams.common.global.AmsGlobalHolder.getPackageName;
 
 /**
  * +----------------------------------------------------------------------
@@ -62,25 +52,25 @@ public class MyMessageReceiver extends MessageReceiver {
     /**
      * 接受到对应消息后，消息的弹出处理
      */
-    public void buildNotification(Context context, CPushMessage message) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_view);
-        remoteViews.setImageViewResource(R.id.custom_icon, R.drawable.ic_launcher);
-        remoteViews.setTextViewText(R.id.tv_custom_title, message.getTitle());
-        remoteViews.setTextViewText(R.id.tv_custom_content, message.getContent());
-        remoteViews.setTextViewText(R.id.tv_custom_time, new SimpleDateFormat("HH:mm").format(new Date()));
-        Notification notification = new NotificationCompat.Builder(context)
-                .setContent(remoteViews)
-                .setContentTitle(message.getTitle())
-                .setContentText(message.getContent())
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setDefaults(Notification.DEFAULT_VIBRATE)
-                .setPriority(Notification.PRIORITY_DEFAULT)
-                .build();
-        notification.contentIntent = buildClickContent(context, message);
-        notification.deleteIntent = buildDeleteContent(context, message);
-        notificationManager.notify(message.hashCode(), notification);
-    }
+//    public void buildNotification(Context context, CPushMessage message) {
+//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+//        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_view);
+//        remoteViews.setImageViewResource(R.id.custom_icon, R.drawable.ic_launcher);
+//        remoteViews.setTextViewText(R.id.tv_custom_title, message.getTitle());
+//        remoteViews.setTextViewText(R.id.tv_custom_content, message.getContent());
+//        remoteViews.setTextViewText(R.id.tv_custom_time, new SimpleDateFormat("HH:mm").format(new Date()));
+//        Notification notification = new NotificationCompat.Builder(context)
+//                .setContent(remoteViews)
+//                .setContentTitle(message.getTitle())
+//                .setContentText(message.getContent())
+//                .setSmallIcon(R.drawable.ic_launcher)
+//                .setDefaults(Notification.DEFAULT_VIBRATE)
+//                .setPriority(Notification.PRIORITY_DEFAULT)
+//                .build();
+//        notification.contentIntent = buildClickContent(context, message);
+//        notification.deleteIntent = buildDeleteContent(context, message);
+//        notificationManager.notify(message.hashCode(), notification);
+//    }
     public PendingIntent buildClickContent(Context context, CPushMessage message) {
         Intent clickIntent = new Intent();
         clickIntent.setAction("com.cwgj.build");
